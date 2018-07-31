3f6d9df74cd0b74611cae5e36aca413e8c11e9fe
DawitA.Yohannes <dawit.yohannes@helsinki.fi>
2018-07-31 18:15:32 +0300

    modified runDaAnalysis function reporting

--- R/RepDaAnalysisFns.R
+++ R/RepDaAnalysisFns.R
@@ -2087,8 +2087,8 @@ runDaAnalysis <- function(repSeqObj,clusterby="NT",kmerWidth=4,paired=T,clusterD
   
   if(length(allCandidateClones) > 0 ){
     
-    allnRepeats <- length(cDaClonotypesList)
-    nRepeatsWithHits <- length(cDaClonotypesList) - length(is.null(cDaClonotypesList))
+    allnRepeats <- nRepeats
+    nRepeatsWithHits <- nRepeats - length(cDaClonotypesList)
     
     cat("Candidate DA CDR3s detected from ",nRepeatsWithHits,"out of",allnRepeats,"repeat resamples.","\n Number of candidate CDR3 clonotypes detected as differentially abundant before filtering: ",length(allCandidateClones),"\n")
   }else{
