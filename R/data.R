6a65025fc230273362ef23881aeb2282c9b992f8
DawitA.Yohannes <dawit.yohannes@helsinki.fi>
2018-08-01 12:04:20 +0300

    added cdpbmc data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Celiac disease (CD) PBMC Repseq data object
+#'
+#' The CD PBMC Repseq data object is a list with three elements. First element contains the 
+#' list of 8 immune repertoire samples, second element contains a vector of the names of the 
+#' samples, third element contains a vector of their condition status in zeros and ones.
+#' The object is prepared by the setUp function as described in the help of setUp or on RepAn github ReadMe.
+#' 
+#' @format A list with 3 elements: \code{sampleData}, \code{samNames} and
+#'   \code{group}
+"CDRepseqObj"
+
---
+++ data-raw/cdpbmcdata.R
@@ -0,0 +1,26 @@
+
+# Including data reading and analysis funtions
+source("/wrk/dawit/TCRanalysis/RepAnalysis26072018/RepAn/R/RepAnFns.R")
+source("/wrk/dawit/TCRanalysis/RepAnalysis26072018/RepAn/R/RepDaAnalysisFns.R")
+
+
+
+# read in data
+CD005d0W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD005d0W.tsv")
+CD005d6W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD005d6W.tsv")
+CD006d0W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD006d0W.tsv")
+CD006d6W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD006d6W.tsv")
+
+CD011d0W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD011d0W.tsv")
+CD011d6W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD011d6W.tsv")
+CD039d0W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD039d0W.tsv")
+CD039d6W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD039d6W.tsv")
+
+
+# Prepare data object for analysis, and use setUp to get a repseq object
+
+samNames=c("CD005d0W","CD006d0W","CD011d0W","CD039d0W","CD005d6W","CD006d6W","CD011d6W","CD039d6W")
+
+CDRepseqObj <- setUp(sampleNames=samNames,samGroup=c(0,0,0,0,1,1,1,1))
+
+devtools::use_data(CDRepseqObj, overwrite = TRUE)
