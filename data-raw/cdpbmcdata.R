af948d6ac049efd41138c410b189cc0cc2abdd7a
DawitA.Yohannes <dawit.yohannes@helsinki.fi>
2018-08-01 12:06:30 +0300

    added cdpbmc data

--- data-raw/cdpbmcdata.R
+++ data-raw/cdpbmcdata.R
@@ -3,8 +3,6 @@
 source("/wrk/dawit/TCRanalysis/RepAnalysis26072018/RepAn/R/RepAnFns.R")
 source("/wrk/dawit/TCRanalysis/RepAnalysis26072018/RepAn/R/RepDaAnalysisFns.R")
 
-
-
 # read in data
 CD005d0W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD005d0W.tsv")
 CD005d6W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD005d6W.tsv")
@@ -18,9 +16,7 @@ CD039d6W=readSample("/wrk/dawit/TCRanalysis/data/PBMC/CD039d6W.tsv")
 
 
 # Prepare data object for analysis, and use setUp to get a repseq object
-
 samNames=c("CD005d0W","CD006d0W","CD011d0W","CD039d0W","CD005d6W","CD006d6W","CD011d6W","CD039d6W")
-
 CDRepseqObj <- setUp(sampleNames=samNames,samGroup=c(0,0,0,0,1,1,1,1))
 
 devtools::use_data(CDRepseqObj, overwrite = TRUE)
